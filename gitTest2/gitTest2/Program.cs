b4447c1b6f4e18109e030e6ae8dda95448ddf7be
itlsamuel <italosamuel0@gmail.com>
2022-02-22 18:03:34 -0300

    test2

---
+++ gitTest2/gitTest2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitTest2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("gisttest2");
+            Console.ReadKey();
+        }
+    }
+}
